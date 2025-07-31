1ee63da5ec6744c4f7eb45f9967006b8ddaef768
Hibasyed11 <hibasyed118@gmail.com>
2025-07-31 10:45:48 -0400

    string operation

---
+++ datatypes.py
@@ -0,0 +1,3 @@
+text=input("Enter a string value")
+revtext=text [::-1]
+print(revtext)
--- input.py
+++ input.py
@@ -1,8 +1,19 @@
-a=5
-print("type of a is", type(a))
-b=2.8
-print("type of b is", type(b))
-c=("Hello")
-print("type of c is", type(c))
-d=True
-print("type of d is", type(d))
+name="penguin"
+print("name ",name)
+print("Type of name is", type(name))
+age=15
+print("age ",age)
+print("Type of age is", type(age))
+is_student=True
+print("is_student ",is_student)
+print("Type of is_student is", type(is_student))
+weight=38.5
+print("weight ",weight)
+print("Type of weight is", type(weight))
+print("After Typecasting")
+age=str(age)
+print("age ",age)
+print("Type of age is", type(age))
+weight=int(weight)
+print("weight ",weight)
+print("Type of weight is", type(weight))
