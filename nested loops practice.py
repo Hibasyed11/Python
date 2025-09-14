1792bbdb346373f896e2029d452886ad3ba3bc1c
Hibasyed11 <hibasyed118@gmail.com>
2025-09-14 06:20:55 -0400

    Extra Nested Loops Practice

---
+++ nested loops practice.py
@@ -0,0 +1,21 @@
+lower= int(input("Input your lower number. "))
+upper= int(input("Input your higher number. "))
+for i in range (lower,upper+1):
+    if i>1:
+        for j in range (2,i):
+            if i%j==0:
+                break
+        else:
+            print(i)
+
+
+for i in range (1,6):
+    for j in range (1,11):
+        print(i,"x",j,"=", i*j)
+    print()
+
+
+for i in range (1,6):
+    for j in range (1,i+1):
+        print(j,end="")
+    print()
