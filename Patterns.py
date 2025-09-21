99181bb7654aa3fddb8387b13c3e11d84de43744
Hibasyed11 <hibasyed118@gmail.com>
2025-09-21 06:23:12 -0400

    Patterns

--- Patterns.py
+++ Patterns.py
@@ -1,7 +1,19 @@
-for i in range (1,11):
-    print(i*"*")
+rowSize=int(input("What is your row size. "))
+if rowSize%2==0:
+    half_DimondRow=rowSize//2
+else:
+    half_DimondRow=rowSize//2+1
 
-i=1
-while i<11:
-    print(i*"*")
-    i+=1
+for i in range (1, half_DimondRow+1):
+    spaces=" "*(half_DimondRow-i)
+    numbers=""
+    for x in range (1,2*i):
+        numbers= numbers +str(x)
+    print(spaces+numbers) 
+
+for i in range (1, half_DimondRow):
+    spaces=" "*i
+    numbers=""
+    for x in range (1,2*(half_DimondRow-i)):
+        numbers= numbers+str(x)
+    print(spaces + numbers)
