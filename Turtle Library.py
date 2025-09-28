54774dcc65608ce7cebfd316c6d1ac44408c8044
Hibasyed11 <hibasyed118@gmail.com>
2025-09-28 06:23:05 -0400

    Beginner Turtle Library Code

---
+++ Turtle Library.py
@@ -0,0 +1,9 @@
+import turtle 
+turtle.Screen().bgcolor("lavender")
+turtle.Screen().setup(200,200)
+turtle.speed(10)
+t=turtle.Turtle()
+for i in range (500):
+    t.forward(i)
+    t.right(91)
+turtle.done()
