164ce1e602bcafcd73526ea7849de1dfa4d4dc48
Hibasyed11 <hibasyed118@gmail.com>
2025-11-02 06:05:21 -0500

    Math Module

---
+++ Math Module.py
@@ -0,0 +1,36 @@
+#Floor and Celing Values
+import math
+print("The celing value of 23.56 is "+str(math.ceil(23.56)))
+print("The floor value of 23.56 is "+str(math.floor(23.56)))
+
+#Copysign
+x=10
+y=-15
+print("The value of x after copying the sign of y is",math.copysign(x,y))
+
+#Using absolute value
+z=-14
+print("The absolute value of z is",math.fabs(z)) 
+
+#Exponentiation
+print(math.pow(4,3))
+
+#Square root
+print(math.sqrt(121))
+
+#Degrees to radian/Radians to degrees
+print(math.radians(80))
+print(math.degrees(1.3962634015954636))
+
+#Factorials
+print(math.factorial(5))
+
+#LCM and GCD
+print(math.lcm(3,5))
+print(math.gcd(6,4))
+
+#Product of Numbers
+print(math.prod([1,3,6,10]))
+
+#Pi
+print(math.pi)
